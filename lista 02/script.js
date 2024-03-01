f5ee5b5b07856131a36952fac6e4223da1c0e5b5
Jeremias Bezerra Lucas <88001432+JeremiasLucas@users.noreply.github.com>
2024-03-01 23:50:03 +0000

    q2 lista2 feita.

---
+++ lista 02/script.js
@@ -0,0 +1,8 @@
+// Array original
+var vet1 = ['Arrays', 'com', 'javaScript'];
+
+// Transformando os elementos em uma frase
+var frase = vet1.join(' ') + ' com JavaScript';
+
+// Exibindo a frase
+console.log(frase);
